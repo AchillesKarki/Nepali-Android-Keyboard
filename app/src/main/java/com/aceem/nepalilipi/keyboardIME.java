8a2eccb959c5967cfb066961dbb1a449fca0d377
Ashim Karki <ashim@sursatech.com>
2018-07-17 16:43:30 +0545

    first push with romanized keyboard layout

---
+++ app/src/main/java/com/aceem/nepalilipi/keyboardIME.java
@@ -0,0 +1,118 @@
+package com.aceem.nepalilipi;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.media.AudioManager;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+/**
+ * Created by aceem on 3/4/2018.
+ */
+
+public class keyboardIME extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView kv;
+    private Keyboard keyboard;
+    private int flag = 0;
+
+    private boolean caps = false;
+
+    @Override
+    public void onPress(int primaryCode) {
+
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+        playClick(primaryCode);
+        switch (primaryCode) {
+            case Keyboard.KEYCODE_DELETE:
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                if (flag == 0) {
+                    keyboard = new Keyboard(this, R.xml.qwerty1);
+                    kv.setKeyboard(keyboard);
+                    kv.setOnKeyboardActionListener(this);
+                    flag = 1;
+                } else {
+                    keyboard = new Keyboard(this, R.xml.qwerty0);
+                    kv.setKeyboard(keyboard);
+                    kv.setOnKeyboardActionListener(this);
+                    flag = 0;
+                }
+                break;
+
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char) primaryCode;
+                ic.commitText(String.valueOf(code), 1);
+        }
+
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+
+    @Override
+    public View onCreateInputView() {
+        kv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);
+        keyboard = new Keyboard(this, R.xml.qwerty0);
+        kv.setKeyboard(keyboard);
+        kv.setOnKeyboardActionListener(this);
+        return kv;
+    }
+
+
+    private void playClick(int keyCode) {
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        switch (keyCode) {
+            case 32:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+                break;
+            case Keyboard.KEYCODE_DONE:
+            case 10:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case Keyboard.KEYCODE_DELETE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+                break;
+            default:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+        }
+    }
+
+}
